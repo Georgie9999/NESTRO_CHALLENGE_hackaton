48417b68452b491032a3918fe5a6706c4799197a
Georgie9999 <ghjch219999@mail.ru>
2023-10-03 22:09:55 +0300

    Немного исправлен dashboard

--- dashboard/dashboard_one.py
+++ dashboard/dashboard_one.py
@@ -2,210 +2,94 @@ import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+import dash_mantine_components as dmc
 from datetime import timedelta, datetime
-from dateutil.relativedelta import *
 
-
+from dash import Output, Input, callback, dash_table
+from dateutil.relativedelta import relativedelta
 
 data = pd.read_excel('../first_table/Приложение 1.xlsx')
 data.columns = data.iloc[0]
 data = data.drop(0, axis=0)
 
 dollar_course = data['Курс $'][data['Курс $'].notna()]
+dollar_course.iloc[0] = 94.11
 dollar_data = data.iloc[:, 13][data.iloc[:, 13].notna()]
+dollar = pd.DataFrame({"Дата": dollar_data, "Курс доллара, руб.": dollar_course})
 
 brent_price = data['Цена Brent,\n$/барр.'][data['Цена Brent,\n$/барр.'].notna()]
-
 spread_price = data['Spread (Корректировка),\n$/барр.'][data['Spread (Корректировка),\n$/барр.'].notna()]
 freight_price = data['Freight (Корректировка),\n$/барр.'][data['Freight (Корректировка),\n$/барр.'].notna()]
 
+price = pd.DataFrame({"Дата": dollar_data, "Цена Brent, $/барр.": brent_price,
+                      "Spread (Корректировка), $/барр.": spread_price,
+                      "Freight (Корректировка), $/барр.": freight_price})
 
-customers = pd.read_csv("../first_table/customers.csv").drop('Unnamed: 0', axis = 1)
-# print(customers)
+customers = pd.read_csv("../first_table/customers.csv").drop('Unnamed: 0', axis=1)
+
 customers_values = customers['Покупатель']
 profit_course = customers['Прибыль за счёт изменения курса (руб)']
 cash_course = customers['Поступление (руб)']
+customers = pd.DataFrame({"Покупатель": customers_values,
+                          "Прибыль за счёт изменения курса (руб)": profit_course,
+                          "Поступление (руб)": cash_course})
 
 urals = pd.read_csv("../first_table/urals.csv")['Urals']
 
-external_stylesheets = [
-    {
-        "href": "https://fonts.googleapis.com/css2?"
-                "family=Lato:wght@400;700&display=swap",
-        "rel": "stylesheet",
-    },
-]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+oil_price_urals = pd.DataFrame(
+    {"Дата_urals": [datetime(2022, 2, 1) + relativedelta(months=+i) for i in range(11)],
+     "Цена Юралс, $/барр.": urals})
+
+oil_price_brent = pd.DataFrame(
+    {"Дата_brent": dollar_data,
+     "Цена Brent, $/барр.": brent_price})
+
+app = dash.Dash(__name__)
 app.title = "Dashboard for 3 case"
 
-app.layout = html.Div(
-    children=[
-        html.Div(
-            children=[
-                html.H1(
-                    children="NESTRO_CHALLENGE_hackaton", className="header-title"
-                ),
+app.layout = html.Div([
+    html.Div(children=[html.H1(children='NESTRO_CHALLENGE_hackaton', style={'textAlign': 'center'}),
+                       html.H2(children='АО «Зарубежнефть»', style={'textAlign': 'center'})]),
 
+    html.Div(children=[html.H2(children='Курс доллара, руб.'),
+                       dash_table.DataTable(data=dollar.to_dict('records'), page_size=6),
+                       dcc.Graph(figure=px.line(dollar, x='Дата', y='Курс доллара, руб.', markers=True,
+                                                color_discrete_sequence=["orange"]))]),
 
-            ],
-            className="header",
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=dcc.Graph(
-                        id="price-chart",
-                        config={"displayModeBar": False},
-                        figure={
-                            "data": [
-                                {
-                                    "x": dollar_data,
-                                    "y": dollar_course,
-                                    "type": "lines",
-                                },
-                            ],
-                            "layout": {
-                                "title": {
-                                    "text": "Курс доллара, руб.",
-                                    "x": 0.45,
-                                    "xanchor": "left",
-                                },
-                                "xaxis": {"fixedrange": True},
-                                "yaxis": {
-                                    "fixedrange": True,
-                                },
-                                "colorway": ["#17B897"],
-                            },
-                        },
-                    ),
-                    className="card",
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        id="price-chart",
-                        config={"displayModeBar": False},
-                        figure={
-                            "data": [
-                                {
-                                    "x": [datetime(2022, 2, 1)+ relativedelta(months=+i) for i in range(11)],
-                                    "y": urals,
-                                    "type": "lines",
-                                },
-                            ],
-                            "layout": {
-                                "title": {
-                                    "text": "Цена нефти Юралс по месяцам(2022), $/барр.",
-                                    "x": 0.45,
-                                    "xanchor": "left",
-                                },
-                                "xaxis": {"fixedrange": True},
-                                "yaxis": {
-                                    "fixedrange": True,
-                                },
-                                "colorway": ["#F12D39"],
-                            },
-                        },
-                    ),
-                    className="card",
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        id="volume-chart",
-                        config={"displayModeBar": False},
-                        figure={
-                            "data": [
-                                {
-                                    "x": dollar_data,
-                                    "y": brent_price,
-                                    "type": "lines",
-                                },
-                            ],
-                            "layout": {
-                                "title": {
-                                    "text": "Цена на нефть марки Brent, $",
-                                    "x": 0.45,
-                                    "xanchor": "left",
-                                },
-                                "xaxis": {"fixedrange": True},
-                                "yaxis": {"fixedrange": True},
-                                "colorway": ["#2d7be1"],
-                            },
-                        },
-                    ),
-                    className="card",
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        id="volume-chart",
-                        config={"displayModeBar": False},
-                        figure={
-                            "data": [
-                                {
-                                    "x": dollar_data,
-                                    "y": brent_price,
-                                    "type": "bar",
-                                    "name": "Brent price, $/барр."
-                                },
-                                {
-                                    "x": dollar_data,
-                                    "y": spread_price,
-                                    "type": "bar",
-                                    "name": "Spread, $/барр.",
-                                },
-                                {
-                                    "x": dollar_data,
-                                    "y": freight_price,
-                                    "type": "bar",
-                                    "name": "Freiqht, $/барр.",
-                                },
-                            ],
-                            "layout": {
-                                "title": {
-                                    "text": "Гистограмма изменения котировок, $/ барр.",
-                                    "x": 0.45,
-                                    "xanchor": "left",
-                                },
-                            },
-                        },
-                    ),
-                    className="card",
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        id="volume-chart",
-                        config={"displayModeBar": False},
-                        figure={
-                            "data": [
-                                {
-                                    "x": customers_values,
-                                    "y": profit_course,
-                                    "type": "bar",
-                                    "name": "Прибыль за счёт изменения курса (руб)"
-                                },
-                                {
-                                    "x": customers_values,
-                                    "y": cash_course,
-                                    "type": "bar",
-                                    "name": "Поступление (руб)",
-                                },
-                            ],
-                            "layout": {
-                                "title": {
-                                    "text": "Гистограмма поступления денежных средств с учетом курса, руб.",
-                                    "x": 0.40,
-                                    "xanchor": "left",
-                                },
-                            },
-                        },
-                    ),
-                    className="card",
-                ),
+    dmc.Grid([
+        dmc.Col([html.H2(children='Цена нефти Юралс по месяцам(2022), $/барр.'),
+                 dcc.Graph(figure=px.line(oil_price_urals,
+                                          x="Дата_urals", y="Цена Юралс, $/барр.", markers=True,
+                                          color_discrete_sequence=["green"]))
+                 ], span=6),
+        dmc.Col([html.H2(children='Цена на нефть марки Brent, $'),
+                 dcc.Graph(figure=px.line(oil_price_brent, x="Дата_brent",
+                                          y="Цена Brent, $/барр.", markers=True))
+                 ], span=6),
+    ]),
 
-            ],
-            className="wrapper",
-        ),
-    ]
-)
+    dmc.Grid([
+        dmc.Col([html.H2(children='Гистограмма поступления денежных средств с учетом курса, руб.'),
+                 dash_table.DataTable(data=customers.to_dict('records'), page_size=11,
+                                      style_table={'overflowX': 'auto'})
+                 ], span=6),
+        dmc.Col([
+            dcc.Graph(figure=px.histogram(customers, x='Покупатель',
+                                          y=['Прибыль за счёт изменения курса (руб)',
+                                             "Поступление (руб)"],barmode='group'))
+        ], span=6),
+    ]),
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+    html.Div(children=[html.H2(children='Гистограмма изменения котировок, $/ барр.'),
+                       dash_table.DataTable(data=price.to_dict('records'), page_size=11,
+                                            style_table={'overflowX': 'auto'}),
+                       dcc.Graph(figure=px.histogram(price, x='Дата',
+                                                     y=['Цена Brent, $/барр.',
+                                                        "Spread (Корректировка), $/барр.",
+                                                        "Freight (Корректировка), $/барр."], nbins=20,barmode='group'))])
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
